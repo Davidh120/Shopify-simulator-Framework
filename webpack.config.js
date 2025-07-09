00d446531709acd42eb61babf9169ff356828be3
David <88805417+Davidh120@users.noreply.github.com>
2025-07-09 09:17:10 -0500

    refactor: refactor: Updated webpack.config.js to handle bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'main.js',
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource',
       },
     ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  optimization: {
+    minimize: true,
   },
   mode: 'development',
 };
