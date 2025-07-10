cb97a1c5542cd03fa55cf460b976be8b5b534363
David <88805417+Davidh120@users.noreply.github.com>
2025-07-10 18:38:58 -0500

    Revert "Feat/header (#3)" (#4)
    This reverts commit c465a709fd4ab52f305e60338eecff311f2585d9.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,18 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'main.js',
-    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
-        type: 'asset/resource',
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-    }),
-  ],
-  optimization: {
-    minimize: true,
   },
   mode: 'development',
 };
